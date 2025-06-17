a0bf4b23c7a7a700d01e396518d2357c9edf605e
ceriddenn <ceriddenn@gmail.com>
2025-06-17 01:21:32 -0400

    feat(loop): added looping through api res

--- main.py
+++ main.py
@@ -30,30 +30,32 @@ def generate_access_token():
         print("Error generating access token:", response.text)
         return None
 
-def contact_api(): 
+def contact_api(loopAmt): 
+    finalResult = []
+    index = 0
+
     base_url = "https://test.api.amadeus.com/v2/shopping/flight-offers"
     headers = {
         "Authorization": f"Bearer {generate_access_token()}",
         "Content-Type": "application/json"
     }
     # do not change the search parameters except origin destination & depart!
-    search_params = {
+    while index < loopAmt:
+        search_params = {
         "originLocationCode": "NYC",
         "destinationLocationCode": "CHS",
         # 2025 oct 1st
-        "departureDate": "2024-10-01",
+        "departureDate": f"2025-10-{str(loopAmt).zfill(2)}",
         # these two are required.
         "adults": 1,
-        "max": 1
-    }
+        "max": 3
+        }
 
-    response = requests.get(base_url, params=search_params, headers=headers)
+        response = requests.get(base_url, params=search_params, headers=headers)
 
-    print(f"Request URL: {response.url}")
-    print(f"Response Status Code: {response.status_code}")
-    if response.status_code == 200:
-        print("Response JSON:", response.json())
-    else:
-        print("Error:", response.text)
-
-contact_api()
+        index += 1
+        res = response.json()
+        finalResult.append(res)
+        print(f"Response {index}: {res['data'][0]['itineraries'][0]['segments'][0]['carrierCode']}")
+#Code is able to return flight data for one day for x amount of results
+contact_api(1)
